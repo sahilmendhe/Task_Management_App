2f399a38e13e8bdbd1a499bcaccff5d2f446ec0c
sahilmendhe <75027020+sahilmendhe@users.noreply.github.com>
2023-08-28 02:08:47 +0530

    test case added

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders login form when not logged in', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const loginFormElement = screen.getByTestId('login-form');
+  expect(loginFormElement).toBeInTheDocument();
+});
+
+test('renders tasks list when logged in', () => {
+  render(<App />);
+  const loginButton = screen.getByRole('button', { name: /sign in/i });
+  fireEvent.click(loginButton);
+
+  const tasksListElement = screen.getByTestId('tasks-list');
+  expect(tasksListElement).toBeInTheDocument();
+});
+
+test('logs out when sign out button is clicked', () => {
+  render(<App />);
+  const loginButton = screen.getByRole('button', { name: /sign in/i });
+  fireEvent.click(loginButton);
+
+  const signOutButton = screen.getByRole('button', { name: /sign out/i });
+  fireEvent.click(signOutButton);
+
+  const loginFormElement = screen.getByTestId('login-form');
+  expect(loginFormElement).toBeInTheDocument();
 });
